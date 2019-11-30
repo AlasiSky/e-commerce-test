b2cace513aff8075f48dce59b8f78021ebc1e78e
Jhonny <land260526@gmail.com>
2019-11-30 10:44:54 -0500

    Firebase

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch> 
     </div>
   );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCBXJNmMXZNtjZgKUj4r9hkBTSZxAILrVw",
+    authDomain: "e-commerce-26027.firebaseapp.com",
+    databaseURL: "https://e-commerce-26027.firebaseio.com",
+    projectId: "e-commerce-26027",
+    storageBucket: "e-commerce-26027.appspot.com",
+    messagingSenderId: "386097412451",
+    appId: "1:386097412451:web:70ac4bacaa6dfc23473b56",
+    measurementId: "G-15MQG1LFHB"
+  };
+
+  firebase.initializeApp(config);
